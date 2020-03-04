56eda873933652ed8dee411fc0401f97e643e994
EddieSCJ <eddiestudyifs@gmail.com>
2020-03-04 00:24:58 -0300

    Lojinha do Tio Ed - Created my first domain class

--- src/main/java/com/codereddie/lojinha/resources/CategoryResource.java
+++ src/main/java/com/codereddie/lojinha/resources/CategoryResource.java
@@ -1,16 +1,29 @@
 package com.codereddie.lojinha.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codereddie.lojinha.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
  
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest is working";
+	public List<Category> list() {
+		
+		Category category = new Category(1, "Informática");
+		Category category2 = new Category(2, "Eletrodomésticos");
+		
+		List<Category> categories = new ArrayList<Category>();
+		categories.add(category);
+		categories.add(category2);
+		
+		return categories;
 	}
 	
 }
