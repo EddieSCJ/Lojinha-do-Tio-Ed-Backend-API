b083c21a394d41f2023ceb1eba62748369f3343b
EddieSCJ <eddiestudyifs@gmail.com>
2020-03-18 15:32:23 -0300

    Created clientType enum

---
+++ src/main/java/com/codereddie/lojinha/domain/enums/ClientType.java
@@ -0,0 +1,38 @@
+package com.codereddie.lojinha.domain.enums;
+
+public enum ClientType {
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
+	
+	private Integer code;
+	private String description;
+	
+	private ClientType(Integer code, String description) {
+		this.code = code;
+		this.description = description;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public static ClientType ClientTypeToEnum(Integer code) {
+		if(code == null) {
+			return null;
+		}
+		
+		for (ClientType clientType : ClientType.values()) {
+			if(code.equals(clientType.getCode())) {
+				return clientType;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: "+code);
+	}
+	
+	
+}
