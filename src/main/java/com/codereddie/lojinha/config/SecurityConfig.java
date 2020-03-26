0a18e384f1529f2086004f6869e522c693aafd30
EddieSCJ <eddiestudyifs@gmail.com>
2020-03-25 23:18:45 -0300

    Creating AUTHORIZATION

--- src/main/java/com/codereddie/lojinha/config/SecurityConfig.java
+++ src/main/java/com/codereddie/lojinha/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.codereddie.lojinha.security.JWTAuthenticationFilter;
+import com.codereddie.lojinha.security.JWTAuthorizationFilter;
 import com.codereddie.lojinha.security.JWTUtil;
 
 @Configuration
@@ -49,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
--- src/main/java/com/codereddie/lojinha/security/JWTUtil.java
+++ src/main/java/com/codereddie/lojinha/security/JWTUtil.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -36,6 +37,35 @@ public class JWTUtil {
 		return builder.toString();
 	}
 
+	public boolean validToken(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if(username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
 	
+	private Claims getClaims(String token) {
+		// TODO Auto-generated method stub
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		}catch (Exception e) {
+			return null;
+			// TODO: handle exception
+		}
+	}
 	
 }
